c72ca9ca5c44619ae9e5413ce2a5dd72e4bbf8e5
Guard84 <guardsofword@gmail.com>
2024-09-09 20:33:13 +0300

    створення структури

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import { sync as globSync } from 'glob'; // Виправлення імпорту
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    root: 'src',
+    build: {
+      sourcemap: true,
+
+      rollupOptions: {
+        input: globSync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: 'commonHelpers.js',
+        },
+      },
+      outDir: '../dist',
+    },
+    plugins: [injectHTML(), FullReload(['./src/**/*.html'])],
+  };
+});
